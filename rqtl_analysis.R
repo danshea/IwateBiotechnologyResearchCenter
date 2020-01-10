f208071cb31567d8a9e6ce3798925df6763ceb04
Dan Shea <daniel.john.shea@gmail.com>
2020-01-10 13:53:15 +0900

    20200110: Fixed genotype ordering when loading cross object. Order must be (A, H, B)

--- rqtl_analysis.R
+++ rqtl_analysis.R
@@ -10,7 +10,7 @@ library('qtl')
 # Set the working directory
 setwd('/Users/dj-shea/Desktop/Okazaki_lily')
 # Load the F2 population marker data
-mapthis <- read.cross(format='csv', dir='', file='rqtl.csv', genotypes=c("A", "B", "H"))
+mapthis <- read.cross(format='csv', dir='', file='rqtl_tdf1.csv', genotypes=c("A", "H", "B"))
 # Display a summary of the cross object
 summary(mapthis)
 # Examine missing genotype data
@@ -38,9 +38,6 @@ plot(ntyped(mapthis, "mar"), ylab="No. typed individuals", main="No. genotypes b
 nt.bymar <- ntyped(mapthis, 'mar')
 todrop <- names(nt.bymar[nt.bymar < 125])
 mapthis <- drop.markers(mapthis, todrop)
-# NOTE: This removes "AMP0068746" and "AMP0071726" as less then 100 individuals
-# were genotyped with this marker
-
 
 # Identifying duplicate individuals
 cg <- comparegeno(mapthis)
@@ -48,7 +45,7 @@ par(mfrow=c(1,1), las=1)
 hist(cg[lower.tri(cg)], breaks=seq(0, 1, len=101), xlab="No. matching genotypes")
 rug(cg[lower.tri(cg)])
 
-# Usually, in an F2 population individuals will share genotypes at roughly 40% of
+# Usually, in an F2 population indivudals will share genotypes at roughly 40% of
 # markers. Here, we see the population as a whole skews towards closer to 65%.
 # Since all individuals share less than 80% of genotypes with one another, we
 # opt to not remove any individuals from the sample population.
@@ -67,9 +64,12 @@ totmar(mapthis)
 # Examining markers for segregation distortion
 gt <- geno.table(mapthis)
 # Markers showing segregation distortion
-gt[gt$P.value < 0.05/totmar(mapthis),]
-# markers that don't show segregation distortion
-gt[gt$P.value > 0.05/totmar(mapthis),]
+distorted <- gt[gt$P.value < 0.05/totmar(mapthis),]
+
+# Drop markers that show distortion (with the exception of TDF1)
+mapthis <- drop.markers(mapthis, row.names(distorted)[1:length(row.names(distorted))-1])
+totmar(mapthis)
+
 # Pretty much all of the markers fail to follow a 1:2:1 segregation
 # This may be due to the fact that L. x formolongi (AK) was derived from
 # continuously backcrossing an F1 plant (L. forosonanum x L. longiflorum) with
@@ -87,13 +87,23 @@ for(i in 1:3) plot(gfreq[i,], ylab="Genotype frequency", main=c("AA", "AB", "BB"
 # Estimating recombination fractions
 mapthis <- est.rf(mapthis)
 
+# Note the warning message, which indicates that there are numerous markers with
+# likely switched alleles (A ↔ B). This is indicated through pairs of markers
+# that are strongly indicated to be associated but have estimated recombination
+# fractions ≫ 1/2. The checkAlleles() function gives more detailed information
+# on this issue.
+checkAlleles(mapthis, threshold=5, verbose=2)
+
+mapthis <- switchAlleles(mapthis, c('AMP0011936', 'AMP0030207', 'AMP0030208'))
+mapthis <- est.rf(mapthis)
+
 # **** DO NOT RUN THIS PLOT IF YOU HAVE A LOT OF MARKERS! ****
 # A plot of the LOD scores against the estimated recombination fractions
 # for all marker pairs
-# par(mfrow=c(1,1), las=1)
-# rf <- pull.rf(mapthis)
-# lod <- pull.rf(mapthis, what="lod")
-# plot(as.numeric(rf), as.numeric(lod), xlab="Recombination fraction", ylab="LOD score")
+par(mfrow=c(1,1), las=1)
+rf <- pull.rf(mapthis)
+lod <- pull.rf(mapthis, what="lod")
+plot(as.numeric(rf), as.numeric(lod), xlab="Recombination fraction", ylab="LOD score")
 
 # *****************************************************************************
 # NOTE: This part should be run during the initial analysis when you are trying
@@ -105,85 +115,43 @@ mapthis <- est.rf(mapthis)
 # linkage group or chromosome for each marker, and then the assigned linkage
 # group, as inferred from the pairwise linkage information. The inferred linkage
 # groups are numbered in decreasing order of size (so that linkage group 1 has
-# the largest number of markers). Here we see that 11 linkage groups were
-# inferred, with the last 2 groups having just one marker. One may play with
-# max.rf and min.lod until the result is about as expected.
+# the largest number of markers). One may play with max.rf and min.lod until the
+# result is about as expected.
+lg <- formLinkageGroups(mapthis, max.rf=0.35, min.lod=3)
+table(lg[,2])
+#  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 
+# 60 46 28 25 24 18 15 11 10  9  8  8  8  4  3  2  1  1 
 
-lg <- formLinkageGroups(mapthis, max.rf=0.35, min.lod=6)
-table(lg[,2])
-#    1    2    3
-# 5832  411  356
-lg <- formLinkageGroups(mapthis, max.rf=0.40, min.lod=6)
-table(lg[,2])
-#    1    2    3
-# 5832  411  356
-lg <- formLinkageGroups(mapthis, max.rf=0.40, min.lod=9)
-table(lg[,2])
-#    1    2    3
-# 5832  411  356
-lg <- formLinkageGroups(mapthis, max.rf=0.40, min.lod=3)
-table(lg[,2])
-#    1    2    3
-# 5832  411  356
-lg <- formLinkageGroups(mapthis, max.rf=0.25, min.lod=6)
-table(lg[,2])
-#    1    2    3
-# 5832  411  356
-lg <- formLinkageGroups(mapthis, max.rf=0.15, min.lod=6)
-table(lg[,2])
-#    1    2    3    4    5    6    7    8    9   10   11   12   13   14
-# 4193  411  373  356  267  258  221  206  196   44   31    7    6    3
-#   15   16   17   18   19   20   21   22   23   24   25   26   27   28
-#    2    2    2    1    1    1    1    1    1    1    1    1    1    1
-#   29   30   31   32   33   34   35   36   37   38
-#    1    1    1    1    1    1    1    1    1    1
-lg <- formLinkageGroups(mapthis, max.rf=0.20, min.lod=6)
-table(lg[,2])
-#    1    2    3    4    5
-# 5588  411  356  200   44
-lg <- formLinkageGroups(mapthis, max.rf=0.15, min.lod=6)
-table(lg[,2])
-#    1    2    3    4    5    6    7    8    9   10   11   12   13   14
-# 4193  411  373  356  267  258  221  206  196   44   31    7    6    3
-#   15   16   17   18   19   20   21   22   23   24   25   26   27   28
-#    2    2    2    1    1    1    1    1    1    1    1    1    1    1
-#   29   30   31   32   33   34   35   36   37   38
-#    1    1    1    1    1    1    1    1    1    1
-
-# We arrive at using max.rf = 0.15 and a min.lod = 6
+# We arrive at 18 LGs using max.rf = 0.35 and a min.lod = 3
 # We can reorganize the markers into these inferred linkage groups. We do so
 # with the same function, formLinkageGroups(), via the argument reorgMarkers.
-mapthis <- formLinkageGroups(mapthis, max.rf=0.15, min.lod=6, reorgMarkers=TRUE)
+mapthis <- formLinkageGroups(mapthis, max.rf=0.35, min.lod=3, reorgMarkers=TRUE)
 # A plot of the pairwise recombination fractions and LOD scores may indicate how
 # well this worked.
 par(mfrow=c(1,1), las=1)
 plotRF(mapthis, alternate.chrid=TRUE)
 
 # Perform initial marker ordering on LGs (chromosomes) 2-13 (from smallest to largest)
-# LG13 to LG2
-for(i in 13:2) {
-  mapthis <- orderMarkers(mapthis, chr=i, window=4, map.function = 'kosambi', verbose=2)
+# LG1 to LG18
+for(i in 1:18) {
+  mapthis <- orderMarkers(mapthis, chr=i, window=5, map.function = 'kosambi', verbose=2)
 }
 
-# Examine the orders determined for markers on each LG using pull.map()
-pull.map(mapthis, chr=13)
+# Plot an image of the genotypes for the population
+geno.image(mapthis, reorder=TRUE, alternate.chrid=TRUE)
 
-# Now We can plot the LGs using plotMap
-plotMap(mapthis, chr=c(2:13), show.marker.names=FALSE, xlab='Linkage Groups')
-for(i in 2:13) {
+# Plot the Genetic map
+plotMap(mapthis, show.marker.names=FALSE, xlab='Linkage Groups')
+
+# Now We can plot the individual LGs using plotMap
+for(i in 1:18) {
   plotMap(mapthis, chr=i, show.marker.names=TRUE, xlab=sprintf("LG%d", i))
 }
 
 # Save the QTL experiment to file
 write.cross(mapthis, format="csv")
 
-# Final Notes
-# This linkage analysis is not ideal, because the markers show severe 
-# segregation distortion. This could be due to the nature of the parent,
-# possibly the random amplification in GRAS-Di is biased by repetetive 
-# regions of the Lily genome, etc. While biological data is usually not as
-# clean as data sets used in tutorials to describe methodologies, the results
-# obtained from this analysis are not very useful. However, the method used to
-# obtain the results shown here is fine, and follows Professor Bromon's R\QTL
-# documentation, which does utilize a nice clean data set for illustrative 
-# purposes. - DJS
+# Write out the individualLGs
+for(i in 1:18){
+  write.cross(mapthis, format='csv', filestem=sprintf("LG%d", i), chr=i)
+}
